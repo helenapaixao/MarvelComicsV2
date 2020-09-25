bac30bd5e5407da5d1a8af5a24f4bd5a686d20dc
Helena Paixão <hp.helenapaixao@gmail.com>
2020-09-24 23:36:18 -0400

    :fire:feature:add lib modal

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Container, Content } from "./styles";
+import { Container, Content, Footer } from "./styles";
 
 const Card: React.FC = () => {
   return (
     <Container>
       <Content>
-    {/*     <h1>Content</h1> */}
+        {/*     <h1>Content</h1> */}
         <img
           src="http://www.comix.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/9/191872_900x900fabuloso.jpg"
           alt="quadrinho"
         />
       </Content>
+      <Footer>
+          <h1>Detalhes</h1>
+          </Footer>
     </Container>
   );
 };
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -3,12 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   background: #000;
   margin-top: 20px;
-  margin-bottom:20px;
+  margin-bottom: 20px;
   margin-left: 70px;
   height: 250px;
   width: 250px;
   justify-content: space-around;
-
 
   border-radius: 15px;
 `;
@@ -17,14 +16,28 @@ export const Content = styled.div`
   background: #8774;
   margin-left: 25px;
   margin-top: 20px;
-text-align:center;
-  height:200px;
-  width:200px;
+  text-align: center;
+  height: 200px;
+  width: 200px;
   align-self: center;
+  border-radius: 10px;
 
   img {
-      height: 200px;
-      width: 200px;
-      border-radius:10px;
+    height: 200px;
+    width: 200px;
+    border-radius: 10px;
   }
+`;
+
+export const Footer = styled.div`
+  background: #e62429;
+  border-radius: 20px;
+  height: 50px;
+  width: 150px;
+  margin-left:50px;
+  h1 {
+      margin-left:20px;
+      
+  }
+
 `;
